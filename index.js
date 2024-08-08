c814bff0cca7f314ed88d678e4f37293f05b720a
SumitRaj18 <sumitraj7162@gmail.com>
2024-08-08 22:17:40 +0530

    Add existing project files to Git

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
+const cors=require('cors')
 const port = process.env.port || 5000
 const connectToMongo= require('./db')
 connectToMongo();
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept"
-  );
-  next();
-})
+app.use(cors({
+  origin:"*"
+}))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
